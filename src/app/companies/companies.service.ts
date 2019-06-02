866639595cb293385d108fa8d181915186a93157
willehpeh <will_alexander@me.com>
2019-06-02 12:30:10 +0200

    GET services ready

--- src/app/companies/companies.service.ts
+++ src/app/companies/companies.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompaniesService {
 
+  constructor(private http: HttpClient) {}
+
+  getAllCompanies() {
+    return this.http.get('http://localhost:3000/companies');
+  }
+
+  getCompanyById(id: string) {
+    return this.http.get(`http://localhost:3000/companies/${id}`);
+  }
 }
--- src/app/people/people.service.ts
+++ src/app/people/people.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeopleService {
-  
+
+  constructor(private http: HttpClient) {}
+
+  getAllPeople() {
+    return this.http.get('http://localhost:3000/people');
+  }
+
+  getPersonById(id: string) {
+    return this.http.get(`http://localhost:3000/people/${id}`);
+  }
 }
